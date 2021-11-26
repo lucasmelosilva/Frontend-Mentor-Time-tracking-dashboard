08d220158b3ddfc0f49d2a86fdce3f72b3abb5c4
lucas melo <lucas.melo044@gmail.com>
2021-11-26 20:35:53 -0300

    add funçao addActiveClass e removeActiveClass

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,6 +1,18 @@
 const menu = document.querySelector('.menu');
+const itemsMenu = [$('#item1'), $('#item2'), $('#item3')];
+
+function removeActiveClass() {
+  itemsMenu.forEach(item => {
+    item.classList.remove('active');
+  });
+}
+
+function addActiveClass(item) {
+  item.classList.add('active');
+}
 
 menu.addEventListener('click', (e) => {
   const target = e.target;
-  target.classList.toggle('active');
+  removeActiveClass();
+  addActiveClass(target);
 });
