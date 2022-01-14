12d7ef8a69831ca545a05f48dd2c66490e6e46ed
lucas melo <lucasmellosilva97@hotmail.com>
2022-01-13 22:01:11 -0300

    modelagem da classe que irar buscar os dados

--- scripts/services/busca.js
+++ scripts/services/busca.js
@@ -1,19 +1,21 @@
 let dados = [];
 
-fetch('../data.json')
-  .then(response => {
-    if(response.ok) {
-      console.log('esta tudo certo')
-      return response.json();
-    }
-  }).then(dados => {
-    let items = dados.map(dado => dado);
-    
-    let novoArrayDeItems = items.map(item => {
-      return item
-    })
 
-    dados =  [...novoArrayDeItems]
-    dados.forEach(dado => console.log(dado.title))
-  })
-  .catch(error => console.log("Error Error "+ error.mensage))
+class ControllerDatas {
+  static async fetchData() {
+    return await fetch('../data.json')
+      .then(response => {
+        if (response.ok) {
+          console.log('esta tudo certo')
+          return response.json();
+        }
+      }).then(items => items.map(item => item))
+  }
+
+
+  async getData() {
+    let data = await ControllerDatas.fetchData();
+    data.forEach(item => console.log(item.title))
+    return [...data];
+  }
+}
